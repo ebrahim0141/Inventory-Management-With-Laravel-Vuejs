c9bd16a42b958b2a14d5c1e5b69eac09a3cd680b
ebrahim0141 <eme.pust@gmail.com>
2019-11-01 00:08:05 +0600

    added collaborating laravel & vuejs project

---
+++ app/Http/Controllers/Api/EmployeeController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Employee;
+use DB;
+use Image;
+class EmployeeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $employee=Employee::all();
+        return response()->json($employee);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+         'name' => 'required|unique:employees|max:255',
+         'email' => 'required',
+         'phone' => 'required|unique:employees',
+        ]);
+        
+            if($request->photo){
+                   $position = strpos($request->photo, ';');
+                   $sub=substr($request->photo, 0 ,$position);
+                   $ext=explode('/', $sub)[1];
+                   $name=time().".".$ext;
+                   $img=Image::make($request->photo)->resize(240,200);
+                   $upload_path='backend/employee/';
+                   $image_url=$upload_path.$name;
+                   $img->save($image_url);
+
+                   $employee = new Employee;
+                   $employee->name = $request->name;
+                   $employee->email = $request->email;
+                   $employee->phone = $request->phone;
+                   $employee->address = $request->address;
+                   $employee->salary = $request->salary;
+                   $employee->nid = $request->nid;
+                   $employee->joining_date = $request->joining_date;
+                   $employee->photo =  $image_url;
+                   $employee->save();
+            }else{
+                   $employee = new Employee;
+                   $employee->name = $request->name;
+                   $employee->email = $request->email;
+                   $employee->phone = $request->phone;
+                   $employee->address = $request->address;
+                   $employee->salary = $request->salary;
+                   $employee->nid = $request->nid;
+                   $employee->joining_date = $request->joining_date;
+                   $employee->save();
+            }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/{vue_capture?}',function () {
+	    return view('welcome');
+	})->where('vue_capture', '[\/\w\.-]*');
